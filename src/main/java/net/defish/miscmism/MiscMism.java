063ae1c1b5417d4cbde066b50087d64ca64738ca
yeoldefishe <adamj.1271@gmail.com>
2026-07-02 21:27:47 -0600

    changed stuff; jade triangle model not working + maybe the lang?

--- src/main/java/net/defish/miscmism/MiscMism.java
+++ src/main/java/net/defish/miscmism/MiscMism.java
@@ -1,5 +1,7 @@
 package net.defish.miscmism;
 
+import net.defish.miscmism.item.ModItems;
+import net.minecraft.world.item.CreativeModeTabs;
 import org.slf4j.Logger;
 
 import com.mojang.logging.LogUtils;
@@ -32,6 +34,8 @@ public class MiscMism
         // Do not add this line if there are no @SubscribeEvent-annotated functions in this class, like onServerStarting() below.
         NeoForge.EVENT_BUS.register(this);
 
+        ModItems.register(modEventBus);
+
         // Register the item to a creative tab
         modEventBus.addListener(this::addCreative);
         // Register our mod's ModConfigSpec so that FML can create and load the config file for us
@@ -44,7 +48,9 @@ public class MiscMism
 
     // Add the example block item to the building blocks tab
     private void addCreative(BuildCreativeModeTabContentsEvent event) {
-
+if(event.getTabKey() == CreativeModeTabs.INGREDIENTS) {
+    event.accept(ModItems.JADETRIANGLE);
+}
     }
 
     // You can use SubscribeEvent and let the Event Bus discover methods to call
---
+++ src/main/java/net/defish/miscmism/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.defish.miscmism.item;
+
+import net.defish.miscmism.MiscMism;
+import net.minecraft.world.item.Item;
+import net.neoforged.bus.api.IEventBus;
+import net.neoforged.neoforge.registries.DeferredItem;
+import net.neoforged.neoforge.registries.DeferredRegister;
+
+public class ModItems {
+    public static final DeferredRegister.Items ITEMS = DeferredRegister.createItems(MiscMism.MOD_ID);
+
+    public static final DeferredItem<Item> JADETRIANGLE = ITEMS.register("jade_triangle",
+         () -> new Item(new Item.Properties()));
+
+
+
+    public static void register(IEventBus eventbus){
+        ITEMS.register(eventbus);
+    }
+}
